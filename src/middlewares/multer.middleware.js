2f81fc43c07827eb229fc6d3e539b2384260a22c
Beboto <bebotoghosh10@gmail.com>
2025-04-16 14:04:05 +0530

    cloudinary and multer files updated

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// method / middleware to store the file in local storage
+const storage = multer.diskStorage({           
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
