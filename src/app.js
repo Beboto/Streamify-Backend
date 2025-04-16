a236744265f058600d12309d16deeb7811b59ab1
Beboto <bebotoghosh10@gmail.com>
2025-04-16 14:23:04 +0530

    user routes file updated

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// Middlewares
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -13,6 +14,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// Import routes
+import userRoutes from "./routes/user.routes.js"
+
+// Use routes
+app.use("/api/v1/users", userRoutes)
 
 
 
