38bf661213a56e2a3aaba27c7dac1d4ad95f19bc
Beboto <bebotoghosh10@gmail.com>
2025-04-21 18:42:15 +0530

    Updated user.routes.js file

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,14 @@ import { Router } from "express";
 import { registerUser, 
          loginUser,
          logoutUser,
-        refreshAccessToken} from "../controllers/user.controller.js";
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage,
+         getUserChannelProfile,
+         getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,6 +36,16 @@ router.route("/login").post(loginUser)
 // Secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)                 // single file upload for avatar using multer middleware
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)    // similarly for cover image
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)       // use "/:username" as it is mentioned in the 'getUserChannelProfile' function in the controller
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 export default router
