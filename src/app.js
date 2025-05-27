0f48e53e944ddf3eb1ce42ad44832f02b683db0d
Beboto <bebotoghosh10@gmail.com>
2025-05-27 19:16:03 +0530

    Refactor middleware comments for clarity and consistency

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"}))                          // Limit the size of JSON payloads to 16kb
+app.use(express.urlencoded({extended: true, limit: "16kb"}))    // Limit the size of URL-encoded payloads to 16kb
+app.use(express.static("public"))                               // Serve static files from the "public" directory
+app.use(cookieParser())                                         // Parse cookies from incoming requests
 
 // Import routes
 import userRoutes from "./routes/user.routes.js"
